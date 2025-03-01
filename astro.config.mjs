5202d830e1e3831b60aeab91b78f6f99d5a6bb79
allohamora <herman30698@gmail.com>
2025-03-01 12:51:20 +0200

    feat: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig, envField } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelServerless({}),
   env: {
     schema: {
       GEMINI_API_KEY: envField.string({ context: 'server', access: 'secret' }),
